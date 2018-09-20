acc4146fe4439aade59eed9e08181aaa845e28e0
bz0 <bzz0217@yahoo.co.jp>
2018-09-20 22:45:52 +0900

    test

---
+++ tests/File/TsvTest.php
@@ -0,0 +1,15 @@
+<?php
+require_once(dirname(__DIR__) . "/../vendor/autoload.php");
+require_once(dirname(__DIR__) . "/../src/File/Tsv.php");
+
+use PHPUnit\Framework\TestCase;
+
+class StackTest extends TestCase
+{
+    public function testAccept()
+    {
+        $tsv = new bz0\CSVToDB\File\Tsv();
+        $this->assertEquals(false, $tsv->accept("csv"));
+        $this->assertEquals(true, $tsv->accept("tsv"));
+    }
+}
